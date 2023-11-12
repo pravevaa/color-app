a483f069374520db0df1b71dfbe18d37975dea1f
pravevaa <145651629+pravevaa@users.noreply.github.com>
2023-11-12 13:17:07 +0000

    ok

--- app.py
+++ app.py
@@ -39,8 +39,8 @@ def read_file():
 
 if __name__ == "__main__":
     #app.run(host="0.0.0.0", port="8080")
-    from waitress import serve
-    serve(app,host="0.0.0.0", port=8080)
+  from waitress import serve
+   serve(app,host="0.0.0.0", port=8080)
 
 
 #if __name__ == "__main__":
